20edaf15d902868b372b06ca6f48597a894d052d
Max Khamrovskyi <maksim36ua@gmail.com>
2018-01-03 23:49:08 +0200

    Added description with details to channel targeted post

--- Services/DatabaseConnectionService.cs
+++ Services/DatabaseConnectionService.cs
@@ -9,141 +9,149 @@ using System.Web;
 
 namespace Codewars_Bot.Services
 {
-    public class DatabaseConnectionService
-    {
-        public void AuditMessageInDatabase(string message)
-        {
-            var messageModel = new AuditMessageModel
-            {
-                Message = message
-            };
-            using (SqlConnection connection = new SqlConnection(Configuration.DbConnection))
-            {
-                var query = $"INSERT INTO [Audit].[Messages] (Message, DateTime) VALUES (@Message, GETDATE())";
-                connection.Query(query, messageModel);
-            }
+	public class DatabaseConnectionService
+	{
+		public void AuditMessageInDatabase(string message)
+		{
+			var messageModel = new AuditMessageModel
+			{
+				Message = message
+			};
+			using (SqlConnection connection = new SqlConnection(Configuration.DbConnection))
+			{
+				var query = $"INSERT INTO [Audit].[Messages] (Message, DateTime) VALUES (@Message, GETDATE())";
+				connection.Query(query, messageModel);
+			}
 
-        }
+		}
 
-        public string GetWeeklyRating()
-        {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(Configuration.DbConnection))
-                {
-                    var getWeekQuery = $"SELECT TOP 1 * FROM [Rating].[Weeks] ORDER BY WeekNumber DESC";
-                    var previousWeek = connection.Query<WeekModel>(getWeekQuery).First();
+		public string GetWeeklyRating()
+		{
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(Configuration.DbConnection))
+				{
+					var getWeekQuery = $"SELECT TOP 1 * FROM [Rating].[Weeks] ORDER BY WeekNumber DESC";
+					var previousWeek = connection.Query<WeekModel>(getWeekQuery).First();
 
-                    var getUsersRatingQuery = $"SELECT * FROM [Rating].[WeeklyRatingUserModel] WHERE WeekNumber = {previousWeek.WeekNumber}";
-                    var usersWithRating = connection.Query<WeeklyRatingUserModel>(getUsersRatingQuery).ToList();
+					var getUsersRatingQuery = $"SELECT * FROM [Rating].[WeeklyRatingUserModel] WHERE WeekNumber = {previousWeek.WeekNumber}";
+					var usersWithRating = connection.Query<WeeklyRatingUserModel>(getUsersRatingQuery).ToList();
 
-                    StringBuilder response = new StringBuilder($"**Week: {previousWeek.WeekNumber}**<br/>");
+					StringBuilder response = new StringBuilder($"**Рейтинг клану IT KPI на Codewars. Тиждень {previousWeek.WeekNumber}**<br/>");
 
-                    string query = "SELECT * FROM [User].[Users]";
-                    var users = connection.Query<UserModel>(query).ToList();
+					string query = "SELECT * FROM [User].[Users]";
+					var users = connection.Query<UserModel>(query).ToList();
 
-                    var currentWeekUsersRating = new List<UserModel>();
+					var currentWeekUsersRating = new List<UserModel>();
 
-                    foreach (var user in users)
-                    {
-                        var lastWeekUserEntry = usersWithRating.FirstOrDefault(q => q.CodewarsUsername == user.CodewarsUsername);
-                        var lastWeekPoints = lastWeekUserEntry == null ? 0 : lastWeekUserEntry.Points;
+					foreach (var user in users)
+					{
+						var lastWeekUserEntry = usersWithRating.FirstOrDefault(q => q.CodewarsUsername == user.CodewarsUsername);
+						var lastWeekPoints = lastWeekUserEntry == null ? 0 : lastWeekUserEntry.Points;
 
-                        var newUser = new UserModel
-                        {
-                            CodewarsUsername = user.CodewarsUsername,
-                            Points = user.Points - lastWeekPoints,
-                            TelegramUsername = user.TelegramUsername
-                        };
+						var newUser = new UserModel
+						{
+							CodewarsUsername = user.CodewarsUsername,
+							Points = user.Points - lastWeekPoints,
+							TelegramUsername = user.TelegramUsername
+						};
 
-                        currentWeekUsersRating.Add(newUser);
-                    }
+						currentWeekUsersRating.Add(newUser);
+					}
 
-                    currentWeekUsersRating = currentWeekUsersRating.OrderByDescending(q => q.Points).ToList();
-                    var position = 1;
+					currentWeekUsersRating = currentWeekUsersRating.OrderByDescending(q => q.Points).ToList();
+					var position = 1;
 
-                    foreach (var userWithRating in currentWeekUsersRating)
-                    {
-                        response.Append($"{position}) @{userWithRating.TelegramUsername} ({userWithRating.CodewarsUsername.Replace("_", " ")}) - {userWithRating.Points} <br/>");
-                        position++;
-                    }
+					foreach (var userWithRating in currentWeekUsersRating)
+					{
+						if (position <= 10)
+						{
+							response.Append($"{position}) @{userWithRating.TelegramUsername} **({userWithRating.CodewarsUsername.Replace("_", " ")}) - {userWithRating.Points}** <br/>");
+						}
+						else
+						{
+							response.Append($"{position}) @{userWithRating.TelegramUsername} ({userWithRating.CodewarsUsername.Replace("_", " ")}) - {userWithRating.Points} <br/>");
 
-                    return response.ToString();
-                }
-            }
-            catch(Exception ex)
-            {
-                AuditMessageInDatabase($"EXCEPTION: {ex.Message}");
-                return $"Не вдалось дістати рейтинг за тиждень";
-            }
-        }
+						}
+						position++;
+					}
 
-        public string GetGeneralRating()
-        {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(Configuration.DbConnection))
-                {
-                    string query = "SELECT * FROM [User].[Users]";
-                    var users = connection.Query<UserModel>(query).ToList();
-                    StringBuilder response = new StringBuilder();
-                    var position = 1;
+					return response.ToString();
+				}
+			}
+			catch (Exception ex)
+			{
+				AuditMessageInDatabase($"EXCEPTION: {ex.Message}");
+				return $"Не вдалось дістати рейтинг за тиждень";
+			}
+		}
 
-                    foreach (var user in users.OrderByDescending(q => q.Points))
-                    {
-                        response.Append($"@{user.TelegramUsername} ({user.CodewarsUsername.Replace("_", " ")}) - {user.Points} <br/>");
-                        position++;
-                    }
+		public string GetGeneralRating()
+		{
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(Configuration.DbConnection))
+				{
+					string query = "SELECT * FROM [User].[Users]";
+					var users = connection.Query<UserModel>(query).ToList();
+					StringBuilder response = new StringBuilder($"**Рейтинг клану IT KPI на Codewars**<br/>");
+					var position = 1;
 
-                    return response.ToString();
-                }
-            }
-            catch (Exception ex)
-            {
-                AuditMessageInDatabase($"EXCEPTION: {ex.Message}");
-                return $"Не вдалось дістати загальний рейтинг";
-            }
-        }
+					foreach (var user in users.OrderByDescending(q => q.Points))
+					{
+						response.Append($"@{user.TelegramUsername} ({user.CodewarsUsername.Replace("_", " ")}) - {user.Points} <br/>");
+						position++;
+					}
 
-        public string SaveUserToDatabase(UserModel user)
-        {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(Configuration.DbConnection))
-                {
-                    string query = "INSERT INTO [User].[Users](CodewarsUsername,CodewarsFullname,TelegramUsername,TelegramId,DateTime,Points) values(@CodewarsUsername,@CodewarsFullname,@TelegramUsername,@TelegramId,GETDATE(),@Points); SELECT CAST(SCOPE_IDENTITY() as int)";
-                    var ra = connection.Query<int>(query, user).SingleOrDefault();
-                    return $"Реєстрація успішна! Спасибі і хай ваш код завжди компілиться з першого разу :-)";
-                }
-            }
-            catch (Exception ex)
-            {
-                AuditMessageInDatabase($"EXCEPTION: {ex.Message}, CodewarsUser: {user.CodewarsUsername}");
-                return $"Не вдалось створити користувача: {ex.Message}";
-            }            
-        }
+					return response.ToString();
+				}
+			}
+			catch (Exception ex)
+			{
+				AuditMessageInDatabase($"EXCEPTION: {ex.Message}");
+				return $"Не вдалось дістати загальний рейтинг";
+			}
+		}
 
-        public bool CheckIfUserExists(UserModel user)
-        {
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(Configuration.DbConnection))
-                {
-                    string query = "SELECT * FROM [User].[Users]";
-                    var dbUser = connection.Query<UserModel>(query).ToList().FirstOrDefault(q => q.CodewarsUsername == user.CodewarsUsername);
+		public string SaveUserToDatabase(UserModel user)
+		{
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(Configuration.DbConnection))
+				{
+					string query = "INSERT INTO [User].[Users](CodewarsUsername,CodewarsFullname,TelegramUsername,TelegramId,DateTime,Points) values(@CodewarsUsername,@CodewarsFullname,@TelegramUsername,@TelegramId,GETDATE(),@Points); SELECT CAST(SCOPE_IDENTITY() as int)";
+					var ra = connection.Query<int>(query, user).SingleOrDefault();
+					return $"Реєстрація успішна! Спасибі і хай ваш код завжди компілиться з першого разу :-)";
+				}
+			}
+			catch (Exception ex)
+			{
+				AuditMessageInDatabase($"EXCEPTION: {ex.Message}, CodewarsUser: {user.CodewarsUsername}");
+				return $"Не вдалось створити користувача: {ex.Message}";
+			}
+		}
 
-                    if (dbUser != null)
-                    {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                AuditMessageInDatabase($"EXCEPTION: {ex.Message}, CodewarsUser: {user.CodewarsUsername}");
-                return false;
-            }
-        }
-    }
+		public bool CheckIfUserExists(UserModel user)
+		{
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(Configuration.DbConnection))
+				{
+					string query = "SELECT * FROM [User].[Users]";
+					var dbUser = connection.Query<UserModel>(query).ToList().FirstOrDefault(q => q.CodewarsUsername == user.CodewarsUsername);
+
+					if (dbUser != null)
+					{
+						return true;
+					}
+					return false;
+				}
+			}
+			catch (Exception ex)
+			{
+				AuditMessageInDatabase($"EXCEPTION: {ex.Message}, CodewarsUser: {user.CodewarsUsername}");
+				return false;
+			}
+		}
+	}
 }
--- Services/MessageService.cs
+++ Services/MessageService.cs
@@ -19,6 +19,8 @@ namespace Codewars_Bot.Services
 					return GetWeeklyRating();
 				case "/total_rating":
 					return GetGeneralRating();
+				case "/weekly_rating_channel":
+					return GetWeeklyRatingForChannel();
 				case "/start":
 					return ShowFaq();
 				case "/show_faq":
@@ -78,13 +80,21 @@ namespace Codewars_Bot.Services
 			return databaseConnectionService.GetGeneralRating();
 		}
 
+		private string GetWeeklyRatingForChannel()
+		{
+			var databaseConnectionService = new DatabaseConnectionService();
+			var rating = databaseConnectionService.GetWeeklyRating();
+
+			return rating + "<br/><br/>Зареєструватись в клані і почати набирати бали можна тут: @itkpi_codewars_bot. Якщо маєте питання чи баг репорт -- пишіть йому: @maksim36ua";
+		}
+
 		private string ShowFaq()
 		{
 			var response = new StringBuilder();
 
 			response.Append("Вітаємо в клані ІТ КРІ на Codewars! <br/><br/>codewars.com -- це знаменитий сайт з задачами для програмістів, за розв'язок яких нараховуються бали. ");
 			response.Append("От цими балами ми і будемо мірятись в кінці кожного тижня. <br/><br/>Цей бот створений для того, щоб зробити реєстрацію в клані максимально швидкою і приємною. ");
-			response.Append("Щоб долучитись до рейтингу треба: <br/>1) Зареєструватись на https://codewars.com <br/>2) Надіслати сюди ваш нікнейм в Codewars.");
+			response.Append("Щоб долучитись до рейтингу треба: <br/>1) Зареєструватись на https://codewars.com <br/>2) Надіслати сюди ваш нікнейм в Codewars. ");
 			response.Append("<br/><br/>Бали оновлюються раз на годину. Також доступні дві команди: <br/>1) /weekly_rating показує поточний рейтинг за цей тиждень. <br/>2) /total_rating відображає загальну кількість балів в кожного користувача");
 			response.Append("<br/><br/>Запрошуйте друзів в клан і гайда рубитись!");
 			response.Append("<br/><br/>P.S: якщо знайшли багу або маєте зауваження -- пишіть йому @maksim36ua");
