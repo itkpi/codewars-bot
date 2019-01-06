7016e24754831649180981eaf15d8eae6826b7ba
Kostyantyn Sharovarsky <kostya9@gmail.com>
2019-01-07 00:47:37 +0200

    Add initial faq integration test

---
+++ ITKPI.CodwarsBot.Tests/FaqTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Codewars_Bot.Contracts;
+using Codewars_Bot.Services;
+using FluentAssertions;
+using Microsoft.Bot.Connector;
+using Xunit;
+
+namespace ITKPI.CodwarsBot.Tests
+{
+    public class FaqTest : IClassFixture<IntegrationTestFixture>
+    {
+        private readonly IMessageService _sut;
+
+        public FaqTest(IntegrationTestFixture fixture)
+        {
+            _sut = fixture.ResolveDependency<IMessageService>(); ;
+        }
+
+        [Fact]
+        public async Task ShouldShowFaq_WhenCommandIsFaq()
+        {
+            var result = await _sut.ProcessMessage(new Activity
+            {
+                Text = "/show_faq",
+                From = new ChannelAccount()
+            });
+
+            result.Count.Should().Be(1);
+
+            result.First().Should().StartWith("Вітаємо в клані ІТ КРІ на Codewars!");
+        }
+    }
+}
